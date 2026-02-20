57a5b122caea94e300e00b262aee6ef96e49047c
TR1-Wisdom <125126363+TR1-Wisdom@users.noreply.github.com>
2026-02-20 17:23:17 +0700

    Initial commit: Modular Quant Architecture with Strict Pylance settings

---
+++ main.py
@@ -0,0 +1,11 @@
+﻿import os
+from dotenv import load_dotenv
+from src.engine.backtest_engine import BacktestEngine
+
+def main():
+    load_dotenv()
+    print("--- Inventory LP & Smart Hedge Backtester Starting ---")
+    # TODO: Initialize modules and run engine
+    
+if __name__ == '__main__':
+    main()
